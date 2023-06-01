903262a08af621137c98dd683cb9b584b4f57096
SammyGulp <133907356+SammyGulp@users.noreply.github.com>
2023-06-01 00:40:13 -0700

    gpt save me

--- allTheJsINeed.js
+++ allTheJsINeed.js
@@ -2,3 +2,20 @@ function changeText(val) {
     document.getElementById(content).textContent = val;
     return;
   }
+
+
+  function openRandomWebsite() {
+            // List of websites (add your own)
+            var websites = [
+                "https://www.example1.com",
+                "https://www.example2.com",
+                "https://www.example3.com"
+            ];
+            
+            // Select a random website from the list
+            var randomIndex = Math.floor(Math.random() * websites.length);
+            var randomWebsite = websites[randomIndex];
+            
+            // Open the random website in a new tab
+            window.open(randomWebsite, "_blank");
+        }
