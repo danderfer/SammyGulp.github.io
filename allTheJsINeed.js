c25a29ef6f64cc722c2db69f2839505af5e3022d
SammyGulp <133907356+SammyGulp@users.noreply.github.com>
2023-05-31 23:17:23 -0700

    Hope it functions

--- allTheJsINeed.js
+++ allTheJsINeed.js
@@ -1,4 +1,4 @@
-function changeText(text) {
-    document.getElementById("myText").textContent = text; // Change the text content to "New Text"
+function changeText(name, text) {
+    document.getElementById(name).textContent = text; // Change the text content to "New Text"
     return;
   }
